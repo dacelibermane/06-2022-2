96b39c47d40707deb96545dd8d6b627c50f4e178
dacelibermane <dace.libermane@gmail.com>
2022-11-07 09:45:44 +0200

    first commit

--- 10-classes-and-objects/01.php
+++ 10-classes-and-objects/01.php
@@ -1,1 +1,37 @@
 <?php
+
+class Product
+{
+    private string $name;
+    private float $startPrice;
+    private int $amount;
+
+    public function __construct($name, $startPrice, $amount)
+    {
+        $this->name = $name;
+        $this->startPrice = $startPrice;
+        $this->amount = $amount;
+
+    }
+
+    function main()
+    {
+        $this->printProduct($this->name, $this->startPrice, $this->amount);
+    }
+
+    function printProduct($name, $startPrice, $amount)
+    {
+        echo "$name, price $startPrice, amount $amount.";
+    }
+}
+
+$products = [
+    new Product("Logitech mouse", 70.00, 14),
+    new Product("iPhone 5s", 999.99, 3),
+    new Product("Epson EB-U05", 440.46, 1)
+];
+
+foreach ($products as $product){
+    echo $product->main() . PHP_EOL;
+}
+
