25a2b1b62a19e8083daa67c83129b1a002aafc89
dacelibermane <dace.libermane@gmail.com>
2022-11-07 09:43:56 +0200

    first commit

--- 09-loops/06.php
+++ 09-loops/06.php
@@ -1,9 +1,19 @@
 <?php
 
 $size = 5;
+$starLength = 8;
+$sideLength = 4;
 
 for ($i = 1; $i <= $size; $i++){
-
+    for($j = 1; $j <= $sideLength * $i; $j++){
+        echo " /";
+    }
+    for ($j = 1; $j <= $starLength * $i; $j++){
+        echo "*";
+    }
+    for($j = 1; $j <= $sideLength * $i; $j++){
+        echo " \'";
+    }
 
     echo PHP_EOL;
 }
