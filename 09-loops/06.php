785489be4089d0e3b1edf5a9b424699979164d49
dacelibermane <dace.libermane@gmail.com>
2022-11-07 18:01:11 +0200

    modify inner for loops

--- 09-loops/06.php
+++ 09-loops/06.php
@@ -5,14 +5,14 @@ $starLength = 8;
 $sideLength = 4;
 
 for ($i = 1; $i <= $size; $i++){
-    for($j = 1; $j <= $sideLength * $i; $j++){
-        echo " /";
+    for ($j = 1; $j <= $size * $sideLength - $sideLength * $i; $j++){
+        echo "/";
     }
-    for ($j = 1; $j <= $starLength * $i; $j++){
+    for ($j = 1; $j <= $starLength * $i - $starLength; $j++){
         echo "*";
     }
-    for($j = 1; $j <= $sideLength * $i; $j++){
-        echo " \'";
+    for ($j = 1; $j <= $size * $sideLength - $sideLength * $i; $j++){
+        echo "\\";
     }
 
     echo PHP_EOL;
