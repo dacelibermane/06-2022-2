b6400bd96643c55a4b04cb0e13ee9d5e14736683
dacelibermane <dace.libermane@gmail.com>
2022-11-08 18:26:33 +0200

    first commit

--- 10-classes-and-objects/01.php
+++ 10-classes-and-objects/01.php
@@ -6,24 +6,49 @@ class Product
     private float $startPrice;
     private int $amount;
 
+
     public function __construct($name, $startPrice, $amount)
     {
         $this->name = $name;
         $this->startPrice = $startPrice;
         $this->amount = $amount;
-
     }
 
-    function main()
+    public function getName(): string
     {
-        $this->printProduct($this->name, $this->startPrice, $this->amount);
+        return $this->name;
     }
 
-    function printProduct($name, $startPrice, $amount)
+    public function getStartPrice(): float
     {
-        echo "$name, price $startPrice, amount $amount.";
+        return $this->startPrice;
+    }
+
+    public function getAmount(): int
+    {
+        return $this->amount;
+    }
+
+
+    public function setAmount(int $newAmount): void
+    {
+        $this->amount = $newAmount;
+    }
+
+
+    public function setPrice(float $newPrice): void
+    {
+        $this->startPrice = $newPrice;
+    }
+
+
+    public function printProduct(): string
+    {
+        return $this->getName() . ", " . $this->getStartPrice(). " EUR, " . $this->getAmount() . " units.";
     }
 }
+
+
 
 $products = [
     new Product("Logitech mouse", 70.00, 14),
@@ -31,7 +56,27 @@ $products = [
     new Product("Epson EB-U05", 440.46, 1)
 ];
 
-foreach ($products as $product){
-    echo $product->main() . PHP_EOL;
+foreach ($products as $key => $product){
+    $key += 1;
+    echo  "{$key}. {$product->printProduct()}" . PHP_EOL;
 }
 
+$userSelection = (int)readline("\nEnter number, which product you would like to change \n");
+$userSelection -= 1;
+$selectedProduct =  $products[$userSelection];
+
+echo  $selectedProduct->printProduct();
+echo PHP_EOL;
+$changeSelection = (int)readline("Enter 1 to change price or 2 to change amount\n");
+
+if($changeSelection == 1){
+    $userPrice =  (int)readline("Enter new price: ");
+    $selectedProduct->setPrice($userPrice);
+    echo $selectedProduct->printProduct();
+}
+
+if($changeSelection == 2){
+    $userAmount =  (int)readline("Enter new amount: ");
+    $selectedProduct->setAmount($userAmount);
+    echo $selectedProduct->printProduct();
+}
