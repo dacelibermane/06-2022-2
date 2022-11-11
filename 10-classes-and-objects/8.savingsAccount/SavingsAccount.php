cfe0a7dda17262631e8b8c79d35c7ef15ec5b1e7
dacelibermane <dace.libermane@gmail.com>
2022-11-11 09:42:38 +0200

    first commit

--- 10-classes-and-objects/8.savingsAccount/SavingsAccount.php
+++ 10-classes-and-objects/8.savingsAccount/SavingsAccount.php
@@ -6,10 +6,31 @@ class SavingsAccount
     private float $startingBalance;
     private float $annualInterestRate;
     private float $monthDeposit;
+
+    private float $deposit = 0;
+    private float $withdrawal = 0;
+
     public function __construct($startingBalance,$annualInterestRate, $monthDeposit)
     {
         $this->startingBalance = $startingBalance;
         $this->annualInterestRate = $annualInterestRate;
         $this->monthDeposit = $monthDeposit;
     }
+
+    public function getStartingBalance(): float
+    {
+        return $this->startingBalance;
+    }
+
+    public function amountOfWithdrawal($withdrawal):float
+    {
+       return $this->startingBalance -= $withdrawal;
+    }
+
+    public function amountOfDeposit($deposit):float
+    {
+       return $this->startingBalance -= $deposit;
+    }
+
+
 }
--- 10-classes-and-objects/8.savingsAccount/index.php
+++ 10-classes-and-objects/8.savingsAccount/index.php
@@ -2,10 +2,10 @@
 
 require_once "SavingsAccount.php";
 
-$startingBalance = (float)readline("How much money is in the account?: \n") . PHP_EOL;
+$account = new SavingsAccount
+(
+    (float)readline("How much money is in the account?: \n") . PHP_EOL,
+    (float)readline("Enter the annual interest rate: \n") . PHP_EOL,
+    (float)readline("How long has the account been opened?") . PHP_EOL
+);
 
-$annualRate = (float)readline("Enter the annual interest rate: \n") . PHP_EOL;
-
-$depositMonth = (float)readline("How long has the account been opened?") . PHP_EOL;
-
-$account = new SavingsAccount($startingBalance, $annualRate, $depositMonth);
